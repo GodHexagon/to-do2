3deb29910b80ed4afc5593dcc4d0ea6b0d077625
GodHexagon <god.hexagon.bifry@gmail.com>
2023-08-03 22:58:35 +0900

    modified:   vite-project/src/App.tsx 	modified:   vite-project/src/main.tsx

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,16 +1,38 @@
 import * as React from 'react';
+import { useState } from "react";
+
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
 
-export default function App() {
+export const App = () => {
+  const [add, setAdd] = useState(true);
+
   return (
     <Stack spacing={2} direction="row">
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
+
+    <FormGroup>
+      <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+      <FormControlLabel required control={<Checkbox />} label="Required" />
+      <FormControlLabel disabled control={<Checkbox />} label="Disabled" />
+    </FormGroup>
+
+    <RadioGroup
+    aria-labelledby="demo-radio-buttons-group-label"
+    defaultValue="female"
+    name="radio-buttons-group"
+    >
+    <FormControlLabel value="female" control={<Radio />} label="Female" />
+    <FormControlLabel value="male" control={<Radio />} label="Male" />
+    <FormControlLabel value="other" control={<Radio />} label="Other" />
+    </RadioGroup>
     </Stack>
   );
 } 
