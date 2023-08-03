ee5ca068b1b037a4e93f8f3c46805f4e11aa4064
GodHexagon <god.hexagon.bifry@gmail.com>
2023-08-03 22:26:19 +0900

    関数名を修正

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -5,7 +5,7 @@ import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 
-export default function BasicButtons() {
+export default function App() {
   return (
     <Stack spacing={2} direction="row">
       <Button variant="text">Text</Button>
