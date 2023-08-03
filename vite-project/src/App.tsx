c7c5a93c6401146480068f7a2cc1033c4582f16f
GodHexagon <god.hexagon.bifry@gmail.com>
2023-08-03 23:59:25 +0900

    modified:   vite-project/src/App.tsx

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -10,29 +10,11 @@ import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 
 export const App = () => {
-  const [add, setAdd] = useState(true);
+  const [add, setAdd] = useState(0);
 
   return (
     <Stack spacing={2} direction="row">
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-
-    <FormGroup>
-      <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
-      <FormControlLabel required control={<Checkbox />} label="Required" />
-      <FormControlLabel disabled control={<Checkbox />} label="Disabled" />
-    </FormGroup>
-
-    <RadioGroup
-    aria-labelledby="demo-radio-buttons-group-label"
-    defaultValue="female"
-    name="radio-buttons-group"
-    >
-    <FormControlLabel value="female" control={<Radio />} label="Female" />
-    <FormControlLabel value="male" control={<Radio />} label="Male" />
-    <FormControlLabel value="other" control={<Radio />} label="Other" />
-    </RadioGroup>
+      <Button variant="contained" onClick={() => setAdd(() => add + 1)}>Contained*{add}</Button>
     </Stack>
   );
 } 
