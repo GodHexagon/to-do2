3396694daa81a21b99159257f8e225353643ca14
GodHexagon <god.hexagon.bifry@gmail.com>
2023-08-06 10:00:55 +0900

    modified:   vite-project/src/App.tsx

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -8,13 +8,25 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
+import { Grid } from '@mui/material';
 
 export const App = () => {
   const [add, setAdd] = useState(0);
 
   return (
-    <Stack spacing={2} direction="row">
-      <Button variant="contained" onClick={() => setAdd(() => add + 1)}>Contained*{add}</Button>
-    </Stack>
+    <Grid container sx={{ height: '100vh', width: '100vw'}}>
+      <Grid item xs={12}>
+        <Button variant="contained" onClick={() => setAdd(() => add + 1)}>Contained*{add}</Button>
+        <Button variant="contained" onClick={() => setAdd(() => add + 1)}>Contained*{add}</Button>
+      </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained" onClick={() => setAdd(() => add + 1)}>Contained*{add}</Button>
+        <Button variant="contained" onClick={() => setAdd(() => add + 1)}>Contained*{add}</Button>
+      </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained" onClick={() => setAdd(() => add + 1)}>Contained*{add}</Button>
+        <Button variant="contained" onClick={() => setAdd(() => add + 1)}>Contained*{add}</Button>
+      </Grid>
+    </Grid>
   );
 } 
